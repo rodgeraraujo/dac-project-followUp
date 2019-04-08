cc09d8009510e660eebfe3d0800a38d5d3510ad9
rafaela <rafaellapereyra@gmail.com>
2019-04-08 14:15:22 -0300

    consumer aluno

--- src/main/java/com/ifpb/followup/consumer/ConsumerAluno.java
+++ src/main/java/com/ifpb/followup/consumer/ConsumerAluno.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ifpb.folloup.consumer;
+package com.ifpb.followup.consumer;
 
 import com.ifpb.followup.model.Aluno;
 import java.io.StringReader;
